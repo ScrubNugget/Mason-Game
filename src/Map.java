eae6a63cc584a33e1a3fb07a465f27f883e2afb5
ScrubNugget <48099927+ScrubNugget@users.noreply.github.com>
2019-03-05 07:28:55 -0600

    Ye
    Added descriptions to some of the methods

--- src/Map.java
+++ src/Map.java
@@ -7,14 +7,15 @@ public class Map {
 		for (int x = 0; x < 20; x++) {
 			for (int y = 0; y < 20; y++) {
 				m[x][y] = (int) (Math.random() * 5);
-			}
+			}//This generates a random map of values, for different blocks
 		}
 	}
 
 	public boolean North() {
 		boolean b = false;
 		if (pr - 1 > 0) {
-			if (pr - 1 != 1 && pr - 1 != 2 && pr - 1 != 3 && pr - 1 != 4) {
+			if (pr - 1 != 1 && pr - 1 != 2 && pr - 1 != 3 && pr - 1 != 4) {//As long as the block the player moves to is a zero, 
+				//it'll return boolean b as true, which allows us to coordinate pixel movement in the applet
 				pr++;
 				b = true;
 			}
@@ -55,7 +56,7 @@ public class Map {
 		return b;
 	}
 
-	public void remove() {
+	public void remove() {// this will eventually be how we'll remove blocks when the player "mines" or destroys them
 	}
 
 	//This makes it so I can retrieve specific values from coordinates - MASON
